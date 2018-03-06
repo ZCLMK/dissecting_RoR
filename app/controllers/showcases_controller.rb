5d59ca1b9a3fd88654d86ebb110c55eceaabcbd8
ZCLMK <zacharie.NTC@gmail.com>
2018-03-06 17:11:09 +0100

    Création de l'action create et d'une page new

--- app/controllers/showcases_controller.rb
+++ app/controllers/showcases_controller.rb
@@ -2,4 +2,21 @@ class ShowcasesController < ApplicationController
   def index
     @showcase_items = Showcase.all
   end
+
+  def new
+    @showcase_items = Showcase.new
+  end
+
+  def create
+    @showcase_item = Showcase.new(params.require(:showcase).permit(:title, :subtile, :body))
+
+    respond_to do |format|
+      if @showcase_item.save
+        format.html { redirect_to showcases_path, notice: 'Votre contribution au portfolio a bien été créee .' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
