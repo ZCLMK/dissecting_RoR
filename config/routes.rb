9adf3808b6ba53e7999a48574391666bf8363fba
ZCLMK <zacharie.NTC@gmail.com>
2018-03-08 10:23:03 +0100

    modif des nom de routes et de root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   resources :showcases
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
+  get 'about' => 'pages#about' # equivalent à: get 'about', to: 'pages#about'
+  get 'contact' => 'pages#contact'
 
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'pages#home'
 end
