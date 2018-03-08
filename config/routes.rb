235e6f216cf95a62a311f7b489ca237453e48fe0
ZCLMK <zacharie.NTC@gmail.com>
2018-03-08 10:50:29 +0100

    custom showcase routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :showcases
+  resources :showcases, except: [:show]
+  get 'showcase/:id' => 'showcases#show', as: 'portfolio_show'
   get 'about' => 'pages#about' # equivalent à: get 'about', to: 'pages#about'
   get 'contact' => 'pages#contact'
 
