a18c4d30dae9d260133cc38e1c8db6b56596cad7
ZCLMK <zacharie.NTC@gmail.com>
2018-03-08 12:05:25 +0100

    debut implementation friendly_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :showcases, except: [:show]
-  get 'showcase/:id' => 'showcases#show', as: 'portfolio_show'
+  get 'showcase/:id' => 'showcases#show', as: 'portfolio_show' #'as:' => Prefixe personnalisé pour ce controller (rediriger d'éventuels liens) dans la view.
   get 'about' => 'pages#about' # equivalent à: get 'about', to: 'pages#about'
   get 'contact' => 'pages#contact'
 
